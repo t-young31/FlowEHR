6d1fb57f067e8757550f0781e943c3a486ed6535
Tom Young <tom.young.316@gmail.com>
2023-01-20 16:47:49 +0000

    init pr comment workflow

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -59,4 +59,4 @@ fi
 MGMT_RG=$("${script_dir}/name.sh" management rg)
 MGMT_STORAGE=$("${script_dir}/name.sh" management str)
 
-export MGMT_RG MGMT_STORAGE
+export MGMT_RG MGMT_STORAGE STATE_CONTAINER="tfstate"
